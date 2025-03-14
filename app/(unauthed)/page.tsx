796e6b4ea21719a3ea50578cefd1e0e9e43a1369
Patticatti <xpatticattix@gmail.com>
2025-03-13 22:48:47 -0700

    Logged in state for button

--- app/(unauthed)/page.tsx
+++ app/(unauthed)/page.tsx
@@ -1,33 +1,51 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { IoLogoGithub } from "react-icons/io";
+import { createClient } from "@/utils/supabase/server";
 import LoginButton from "@/components/login-button";
+import { MoveRight } from "lucide-react";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
   return (
     <div className="flex flex-col items-center px-4 md:px-6 lg:px-8">
       <h1 className="mb-8 font-manrope pt-24 font-semibold text-7xl tracking-tight max-w-2xl text-center mx-auto">
         Generate a Changelog using AI
       </h1>
-      <div className="mt-8 relative flex mx-auto max-w-xl w-full">
-        <IoLogoGithub className="absolute left-3 w-6 h-6 top-1/2 -translate-y-1/2 text-muted-foreground" />
-        <Input
-          className="relative !p-6 !ps-12 placeholder:text-lg !text-lg rounded-tr-none rounded-br-none"
-          placeholder="Enter a public repo URL"
-        />
-        <Button className="bg-blue-600 hover:bg-blue-800 cursor-pointer !min-h-full text-lg py-6 rounded-tl-none rounded-bl-none">
-          Generate
-        </Button>
-      </div>
+      {user ? (
+        <Link href="/dashboard">
+          <Button className="relative text-lg p-6 !px-6 gap-3 cursor-pointer w-full lg:w-fit">
+            Go to dashboard
+            <MoveRight />
+          </Button>
+        </Link>
+      ) : (
+        <>
+          <div className="mt-8 relative flex mx-auto max-w-xl w-full">
+            <IoLogoGithub className="absolute left-3 w-6 h-6 top-1/2 -translate-y-1/2 text-muted-foreground" />
+            <Input
+              className="relative !p-6 !ps-12 placeholder:text-lg !text-lg rounded-tr-none rounded-br-none"
+              placeholder="Enter a public repo URL"
+            />
+            <Button className="bg-blue-600 hover:bg-blue-800 cursor-pointer !min-h-full text-lg py-6 rounded-tl-none rounded-bl-none">
+              Generate
+            </Button>
+          </div>
 
-      <div className="flex items-center gap-3 my-4">
-        <div className="w-48 h-[1px] bg-border" />
-        <p>Or</p>
-        <div className="w-48 h-[1px] bg-border" />
-      </div>
-      <div className="relative max-w-xl w-full sm:w-fit">
-        <LoginButton />
-      </div>
+          <div className="flex items-center gap-3 my-4">
+            <div className="w-48 h-[1px] bg-border" />
+            <p>Or</p>
+            <div className="w-48 h-[1px] bg-border" />
+          </div>
+          <div className="relative max-w-xl w-full sm:w-fit">
+            <LoginButton />
+          </div>
+        </>
+      )}
     </div>
   );
 }
