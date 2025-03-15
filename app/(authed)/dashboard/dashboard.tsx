8a94337dd84a52c988b170affff64629f16e632a
Patticatti <xpatticattix@gmail.com>
2025-03-14 20:06:54 -0700

    Finish dashboard

--- app/(authed)/dashboard/dashboard.tsx
+++ app/(authed)/dashboard/dashboard.tsx
@@ -4,12 +4,12 @@ import { RiGithubFill } from "react-icons/ri";
 import { Badge } from "@/components/ui/badge";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { ExternalLink } from "lucide-react";
+import { ExternalLink, GitBranch, GitCommitHorizontal } from "lucide-react";
 
 export default function DashboardPage() {
   return (
     <>
-      <header className="bg-muted/50 flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+      <header className="bg-muted/50 border-b flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
         <div className="flex items-center px-4">
           <SidebarTrigger className="-ml-1 group-has-data-[collapsible=icon]/sidebar-wrapper:flex hidden opacity-60" />
           <Separator
@@ -27,7 +27,7 @@ export default function DashboardPage() {
       </header>
       <div className="flex bg-muted/50 flex-1 flex-col items-center gap-4 pt-0">
         <div className="p-4 w-full max-w-5xl">
-          <div className="flex flex-col lg:flex-row items-center gap-6 bg-background rounded-xl border p-6">
+          <div className="flex flex-col lg:flex-row items-center gap-6 bg-background rounded-xl border p-6 overflow-hidden">
             <Image
               src="/preview-changelog.png"
               width={700}
@@ -35,37 +35,68 @@ export default function DashboardPage() {
               className="object-top object-cover w-full lg:w-[443px] h-[296px] lg:max-w-1/2 rounded-lg border-4"
               alt="Changelog Preview"
             />
-            <div className="space-y-5 w-full">
-              <label className="font-normal text-muted-foreground text-sm mb-1">
-                Deployment
-              </label>
-              <Button
-                variant="link"
-                className="p-0 h-fit flex items-center gap-2 text-foreground font-semibold text-sm"
-              >
-                <p>new-startup-app.versionary.dev</p>
-                <span className="bg-teal-500 w-4 h-4 shrink-0 rounded-full border-4 border-teal-100" />
-              </Button>
-              <label className="font-normal text-muted-foreground text-sm mb-1">
-                Last Updated
-              </label>
-              <Button
-                variant="link"
-                className="p-0 h-fit flex items-center gap-2 text-foreground text-sm"
-              >
-                <p className="font-normal">2h ago by the-sigma-gamer</p>
-                <span className="bg-teal-500 w-4 h-4 shrink-0 rounded-full border-4 border-teal-100" />
-              </Button>
-              <label className="font-normal text-muted-foreground text-sm mb-1">
-                Domain
-              </label>
-              <Button
-                variant="link"
-                className="!px-0 py-0 h-fit flex items-center gap-2 text-foreground font-semibold text-sm"
-              >
-                <p>www.patticatti.com/changelog</p>
-                <ExternalLink className="size-4 shrink-0" />
-              </Button>
+            <div className="flex flex-col gap-5 min-w-0 overflow-hidden">
+              <div>
+                <div className="font-normal text-muted-foreground text-sm mb-1">
+                  Deployment
+                </div>
+                <Button
+                  variant="link"
+                  className="p-0 h-fit flex items-center gap-2 text-foreground font-semibold text-sm"
+                >
+                  <p>new-startup-app.versionary.dev</p>
+                  <span className="bg-teal-500 w-4 h-4 shrink-0 rounded-full border-4 border-teal-100" />
+                </Button>
+              </div>
+              <div>
+                <div className="font-normal text-muted-foreground text-sm mb-1">
+                  Last Updated
+                </div>
+                <Button
+                  variant="link"
+                  className="p-0 h-fit flex items-center gap-2 text-foreground text-sm"
+                >
+                  <p className="font-normal">2h ago by the-sigma-gamer</p>
+                  <span className="bg-teal-500 w-4 h-4 shrink-0 rounded-full border-4 border-teal-100" />
+                </Button>
+              </div>
+              <div>
+                <div className="font-normal text-muted-foreground text-sm mb-1">
+                  Domain
+                </div>
+                <Button
+                  variant="link"
+                  className="!px-0 py-0 h-fit flex items-center gap-2 text-foreground font-semibold text-sm"
+                >
+                  <p>www.patticatti.com/changelog</p>
+                  <ExternalLink className="size-4 shrink-0" />
+                </Button>
+              </div>
+              <div>
+                <div className="font-normal text-muted-foreground text-sm mb-2">
+                  Source
+                </div>
+                <div className="space-y-2 mt-1">
+                  <Button
+                    variant="link"
+                    className=" !px-0 py-0 h-fit flex items-center gap-3 text-foreground font-semibold text-sm"
+                  >
+                    <GitBranch className="size-4 shrink-0 opacity-70" />
+                    <p className="font-mono font-normal">main</p>
+                  </Button>
+                  <Button
+                    variant="link"
+                    className="!justify-star w-full !px-0 py-0 h-fit flex font-normal items-center gap-3 text-foreground max-w-sm text-sm"
+                  >
+                    <GitCommitHorizontal className=" size-4 shrink-0 opacity-70" />
+                    <p className="font-mono">8f5a489</p>
+                    <p className="truncate ">
+                      Merge pull request #8 from
+                      Patticatti/patti/add-signin-modal
+                    </p>
+                  </Button>
+                </div>
+              </div>
             </div>
           </div>
           <div className="bg-muted/50 aspect-video rounded-xl" />
