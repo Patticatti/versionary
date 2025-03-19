f904d7185542c080d7a2f79a3255dd2aaba5bbdc
Patticatti <xpatticattix@gmail.com>
2025-03-19 15:51:30 -0700

    Fix repo type

--- db/types.ts
+++ db/types.ts
@@ -1,10 +1,10 @@
 export interface Repo {
-  id: string;
+  id?: string;
   user_id: string;
   github_id: number;
   name: string;
   owner: { login: string };
   html_url: string;
-  created_at: string | Date;
-  private: boolean;
+  created_at?: string | Date;
+  private?: boolean;
 }
--- utils/github/clientActions.ts
+++ utils/github/clientActions.ts
@@ -2,8 +2,9 @@
 
 import { SetStateAction } from "react";
 import { createClient } from "../supabase/client";
+import { useZustandStore } from "@/state/zustandStore";
 
-export default async function updateRepository({
+export default function updateRepository({
   github_id,
   user_id,
   name,
@@ -18,32 +19,38 @@ export default async function updateRepository({
   html_url: string;
   setLoading: (value: SetStateAction<boolean>) => void;
 }) {
-  const supabase = createClient();
-  setLoading(true);
+  const { setRepos, repos } = useZustandStore();
+  return new Promise<void>(async (resolve, reject) => {
+    const supabase = createClient();
+    setLoading(true);
 
-  try {
-    const repositoryData = {
-      user_id,
-      github_id,
-      name,
-      owner,
-      html_url,
-      updated_at: new Date().toISOString(),
-    };
+    try {
+      const repositoryData = {
+        user_id,
+        github_id,
+        name,
+        owner: { login: owner },
+        html_url,
+        updated_at: new Date().toISOString(),
+      };
 
-    const { error } = await supabase
-      .from("repositories")
-      .upsert([repositoryData], { onConflict: "github_id" });
+      const { error } = await supabase
+        .from("repositories")
+        .upsert([repositoryData], { onConflict: "github_id" });
 
-    if (error) {
-      console.error("Supabase error:", error.message);
-      throw new Error(error.message);
+      if (error) {
+        console.error("Supabase error:", error.message);
+        throw new Error(error.message);
+      }
+
+      setRepos([...repos, repositoryData]);
+
+      resolve();
+    } catch (error) {
+      console.error("Error updating repository:", error);
+      reject(error);
+    } finally {
+      setLoading(false);
     }
-
-    alert("Repository updated!");
-  } catch (error) {
-    console.error("Error updating repository:", error);
-  } finally {
-    setLoading(false);
-  }
+  });
 }
